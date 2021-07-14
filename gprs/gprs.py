4f94d28d39f65f5302677b2484f7e06eb648b808
Ying-Chu Lo <yingchu.lo@gmail.com>
2021-07-13 18:35:17 -0700

    add if condition in build_prs

--- gprs/gprs.py
+++ gprs/gprs.py
@@ -149,7 +149,7 @@ class GPRS( object ):
         for nb in range( 1, 23 ):
             chrnb = "chr{}".format( nb )
             for vcf_file in os.listdir( vcf_input ):
-                if "vcf" in vcf_file and chrnb != "chrY" and chrnb != "chrX" and chrnb != "wgs" and "{}{}".format(chrnb, symbol)in vcf_file:
+                if "vcf" in vcf_file and ".tbi" not in vcf_file and chrnb != "chrY" and chrnb != "chrX" and chrnb != "wgs" and "{}{}".format(chrnb, symbol)in vcf_file:
                     qc_file = "{}/{}_{}.qc_clump_snpslist.csv".format( self.qc_clump_snpslist_dir(), chrnb, output_name )
                     os.system("plink2 --vcf {}/{} dosage=DS --score {} {} '{}' --out {}/{}_{}".format( self.ref, vcf_file,
                                                                                                  qc_file, columns,
